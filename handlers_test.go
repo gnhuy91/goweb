8fd9f0e4b27c5bb9a948e01c120923da8a491dd8
Huy Giang <gnhuy91@gmail.com>
2016-07-04 11:00:42 +0700

    use go-testdb pkg to fake db for testing handlers without a real db

--- handlers_test.go
+++ handlers_test.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/erikstmartin/go-testdb"
 )
 
 func TestUserList_StatusOK(t *testing.T) {
 	url := "/users"
 
-	// Open our connection and setup our handler
-	db, err := Connect(dbDriver, initDSN())
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// Open fake db from go-testdb
+	db, _ := Connect("testdb", "")
 	defer db.Close()
+
+	// Stub the query
+	query := "select * from user_info"
+	columns := []string{"id", "first_name", "last_name", "email"}
+	result := ``
+	testdb.StubQuery(query, testdb.RowsFromCSVString(columns, result))
 
 	req, _ := http.NewRequest("GET", url, nil)
 
