d90508161e5319160133f4d6770a330c5969d71e
Huy Giang <gnhuy91@gmail.com>
2016-07-05 17:04:55 +0700

    WIP

--- handlers_test.go
+++ handlers_test.go
@@ -4,18 +4,51 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
-func TestUserList_StatusOK(t *testing.T) {
-	url := "/users"
+var db *DB
 
-	// Open our connection and setup our handler
+func TestMain(m *testing.M) {
 	db, err := Connect(dbDriver, initDSN())
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer db.Close()
+
+	// Generate Schema
+	log.Println("Generate DB Schema...")
+	if _, err := db.Exec(schema); err != nil {
+		log.Println(err)
+	}
+
+	// setup()
+
+	code := m.Run()
+
+	// shutdown()
+
+	os.Exit(code)
+}
+
+func setup() {
+
+}
+
+func shutdown() {
+
+}
+
+func TestUserList_StatusOK(t *testing.T) {
+	url := "/users"
+
+	// Open our connection and setup our handler
+	// db, err := Connect(dbDriver, initDSN())
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// defer db.Close()
 
 	req, _ := http.NewRequest("GET", url, nil)
 
