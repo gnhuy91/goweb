56f639cfb11ab0a7970744c87e8ce38fdc2b2c82
Huy Giang <gnhuy91@gmail.com>
2016-07-11 14:06:46 +0700

    use async version of miragte Up/Down to track which `.sql` files has been migrated. example usage of async migrate: https://github.com/mattes/migrate/blob/master/main.go#L167

--- package_test.go
+++ package_test.go
@@ -6,7 +6,10 @@ import (
 	"testing"
 
 	_ "github.com/mattes/migrate/driver/postgres"
+	"github.com/mattes/migrate/file"
 	"github.com/mattes/migrate/migrate"
+	"github.com/mattes/migrate/migrate/direction"
+	"github.com/mattes/migrate/pipe"
 )
 
 var db *DB
@@ -29,22 +32,61 @@ func TestMain(m *testing.M) {
 
 func setup() {
 	fmt.Println("Create DB Schema...")
-	allErrors, ok := migrate.UpSync(configDSN(), migrationsDir)
+
+	p := pipe.New()
+	go migrate.Up(p, configDSN(), migrationsDir)
+	ok := writePipe(p)
 	if !ok {
-		fmt.Println("DB migrate Up failed ...")
-		for _, err := range allErrors {
-			fmt.Println(err)
-		}
+		panic("DB migrate Up failed.")
 	}
 }
 
 func teardown() {
-	fmt.Println("Drop DB Schema...")
-	allErrors, ok := migrate.DownSync(configDSN(), migrationsDir)
+	fmt.Println("\nDrop DB Schema...")
+
+	p := pipe.New()
+	go migrate.Down(p, configDSN(), migrationsDir)
+	ok := writePipe(p)
 	if !ok {
-		fmt.Println("DB migrate Down failed ...")
-		for _, err := range allErrors {
-			fmt.Println(err)
+		panic("DB migrate Down failed.")
+	}
+}
+
+func writePipe(pipe chan interface{}) (ok bool) {
+	okFlag := true
+	if pipe != nil {
+		for {
+			select {
+			case item, more := <-pipe:
+				if !more {
+					return okFlag
+				}
+
+				switch item.(type) {
+				case string:
+					fmt.Println(item.(string))
+
+				case error:
+					fmt.Println(item.(error).Error())
+					okFlag = false
+
+				case file.File:
+					f := item.(file.File)
+					if f.Direction == direction.Up {
+						fmt.Print(">")
+					} else if f.Direction == direction.Down {
+						fmt.Print("<")
+					}
+					fmt.Printf(" %s\n", f.FileName)
+
+				default:
+					text := fmt.Sprint(item)
+					fmt.Println(text)
+				}
+			}
+
+			fmt.Println()
 		}
 	}
+	return okFlag
 }
