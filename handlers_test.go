a60f11c5437055d66b82a0f11129d64ad8d81090
Huy Giang <gnhuy91@gmail.com>
2016-07-15 16:07:21 +0700

    add testParam struct to easily parameterize test inputs

--- handlers_test.go
+++ handlers_test.go
@@ -12,6 +12,18 @@ import (
 	"github.com/gnhuy91/goweb/models"
 )
 
+type testParam struct {
+	BaseURL string
+	UserID  int
+	Method  string
+	Code    int
+}
+
+func (tp *testParam) ReqURL() string {
+	u, _ := concatURL(tp.BaseURL, strconv.Itoa(tp.UserID))
+	return u
+}
+
 func TestUserInsert_ValidBody(t *testing.T) {
 	const (
 		url    = "/user"
@@ -67,20 +79,16 @@ func TestUserInsert_InValidBody(t *testing.T) {
 }
 
 func TestUserByID_StatusOK(t *testing.T) {
-	const (
-		url    = "/user/1"
-		method = "GET"
-		code   = http.StatusOK
-	)
+	p := testParam{"/user", 1, "GET", http.StatusOK}
 
-	req, _ := http.NewRequest(method, url, nil)
+	req, _ := http.NewRequest(p.Method, p.ReqURL(), nil)
 	rec := httptest.NewRecorder()
 
 	NewRouter(db).ServeHTTP(rec, req)
 	errMsg := "%s %s, want %v, got %v"
-	errVars := []interface{}{method, url, code, rec.Code}
+	errVars := []interface{}{p.Method, p.ReqURL(), p.Code, rec.Code}
 
-	if rec.Code != code {
+	if rec.Code != p.Code {
 		t.Errorf(errMsg, errVars...)
 	}
 }
@@ -106,45 +114,51 @@ func TestUserList_StatusOK(t *testing.T) {
 
 func TestUserUpdate(t *testing.T) {
 	const (
-		userID = 1
-		method = "PUT"
-		code   = http.StatusOK
+		baseURL = "/user"
+		method  = "PUT"
+		code    = http.StatusOK
 	)
-	var url = "/user/" + strconv.Itoa(userID)
-
 	body := `{
-			"first_name": "Huy",
-			"last_name": "Giang",
-			"email": "abc@gmail.com"
-		}`
+		"first_name": "Huy",
+		"last_name": "Giang",
+		"email": "abc@gmail.com"
+	}`
 
-	req, _ := http.NewRequest(method, url, strings.NewReader(body))
-	rec := httptest.NewRecorder()
+	// PUT existed user
+	p1 := testParam{baseURL, 1, method, code}
+	// PUT non-existed user
+	p2 := testParam{baseURL, 9999, method, code}
+	ps := []testParam{p1, p2}
 
-	NewRouter(db).ServeHTTP(rec, req)
-	errMsg := "%s %s, body: %s - want %v, got %v"
-	errVars := []interface{}{method, url, body, code, rec.Code}
+	for _, p := range ps {
+		req, _ := http.NewRequest(p.Method, p.ReqURL(), strings.NewReader(body))
+		rec := httptest.NewRecorder()
 
-	if rec.Code != code {
-		t.Errorf(errMsg, errVars...)
-	}
+		NewRouter(db).ServeHTTP(rec, req)
+		errMsg := "%s %s, body: %s - want %v, got %v"
+		errVars := []interface{}{p.Method, p.ReqURL(), body, p.Code, rec.Code}
 
-	// Check if the Update took effect by query the user by ID
-	// and then compare it with the test body.
-	// Both should be parsed to the struct to be able to compare.
-	rec.Flush()
-	req, _ = http.NewRequest("GET", url, nil)
-	NewRouter(db).ServeHTTP(rec, req)
+		if rec.Code != code {
+			t.Errorf(errMsg, errVars...)
+		}
 
-	var userFromTest, userFromDB models.UserInfo
-	json.NewDecoder(rec.Body).Decode(&userFromDB)
+		// Check if the Update took effect by query the user by ID
+		// and then compare it with the test body.
+		// Both should be parsed to the struct to be able to compare.
+		rec.Flush()
+		req, _ = http.NewRequest("GET", p.ReqURL(), nil)
+		NewRouter(db).ServeHTTP(rec, req)
 
-	// manually assign ID here since PUT get ID from url path, not req body
-	userFromTest.ID = userID
-	json.NewDecoder(strings.NewReader(body)).Decode(&userFromTest)
+		var userFromTest, userFromDB models.UserInfo
+		json.NewDecoder(rec.Body).Decode(&userFromDB)
 
-	if userFromDB != userFromTest {
-		t.Errorf("Update %s went wrong, want %+v, got %+v", url, userFromTest, userFromDB)
+		// manually assign ID here since PUT get ID from url path, not req body
+		userFromTest.ID = p.UserID
+		json.NewDecoder(strings.NewReader(body)).Decode(&userFromTest)
+
+		if userFromDB != userFromTest {
+			t.Errorf("Update %s went wrong, want %+v, got %+v", p.ReqURL(), userFromTest, userFromDB)
+		}
 	}
 }
 
