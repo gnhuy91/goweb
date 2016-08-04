908b72e11d92ebc4f59d432b4a97756388b6e10f
Huy Giang <gnhuy91@gmail.com>
2016-08-04 11:04:43 +0700

    update vendor pkgs

--- vendor/github.com/gnhuy91/go-uaa/uaa.go
+++ vendor/github.com/gnhuy91/go-uaa/uaa.go
@@ -4,30 +4,33 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"log"
 	"net/http"
 	"os"
 	"strings"
 )
 
-// UAACheckTokenError is the json structure returned from UAA
+// VerifyError is the json structure returned from UAA
 // if token is invalid
-type UAACheckTokenError struct {
+type VerifyError struct {
 	Error            string `json:"error"`
 	ErrorDescription string `json:"error_description"`
 }
 
-// CheckUAAToken make a post request to uaaCheckTokenURL
+// VerifyToken make a post request to uaaURL
 // to check whether provided token is valid
-func CheckUAAToken(uaaCheckTokenURL, authStr string) (int, error) {
+func VerifyToken(uaaURL, authStr string) (int, error) {
 	// Prepare HTTP request
 	token := strings.TrimPrefix(authStr, "Bearer ")
 	body := []byte("token=" + token)
-	req, err := http.NewRequest("POST", uaaCheckTokenURL, bytes.NewBuffer(body))
+	req, err := http.NewRequest("POST", uaaURL, bytes.NewBuffer(body))
 	if err != nil {
-		log.Println(err)
+		return http.StatusInternalServerError, err
 	}
+
+	// Set Basic Auth header using env variables
+	// TODO: get username & password from parameters
 	req.SetBasicAuth(os.Getenv("UAA_USERNAME"), os.Getenv("UAA_PASSWORD"))
+
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
 	req.Header.Set("Accept", "application/json;charset=utf-8")
 
@@ -35,15 +38,15 @@ func CheckUAAToken(uaaCheckTokenURL, authStr string) (int, error) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Println(err)
+		return http.StatusInternalServerError, err
 	}
 	defer resp.Body.Close()
 
 	// If status code is not 200, parse & return the error msg
 	if resp.StatusCode != http.StatusOK {
-		var payload UAACheckTokenError
+		var payload VerifyError
 		if err := json.NewDecoder(resp.Body).Decode(&payload); err != nil {
-			log.Println(err)
+			return resp.StatusCode, err
 		}
 		return resp.StatusCode, errors.New(payload.Error)
 	}
